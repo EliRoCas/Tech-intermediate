07a47a83f3c751a3378ff0c2b4ea8d1dc65d5526
EliRoCas <elirocas946@gmail.com>
2024-03-22 19:56:06 -0500

    export-import-explanation

---
+++ funcionesR.js
@@ -0,0 +1,28 @@
+function sumar(a, b) {
+    return a + b;
+}
+function restar(a, b) {
+    return a - b;
+}
+function multiplicar(a, b) {
+    return a * b;
+}
+function dividir(a, b) {
+    return a / b;
+}
+function modulo(a, b) {
+    return a % b;
+}
+function exponente(a, b) {
+    return a ** b;
+}
+
+
+export { 
+    sumar, 
+    restar, 
+    multiplicar, 
+    dividir, 
+    modulo, 
+    exponente 
+}
---
+++ operaciones.js
@@ -0,0 +1,15 @@
+import {
+    sumar,
+    restar,
+    multiplicar,
+    dividir,
+    modulo,
+    exponente
+} from './funcionesR.js'
+
+console.log(`La suma de los dos numeros es: ${sumar(10,5)}`);
+console.log(`La resta de los dos numeros es: ${restar(10,5)}`);
+console.log(`La multiplicacion de los dos numeros es: ${multiplicar(10,5)}`);
+console.log(`La division de los dos numeros es: ${dividir(10,5)}`);
+console.log(`El modulo de los dos numeros es: ${modulo(10,5)}`);
+console.log(`El exponente de los dos numeros es: ${exponente(10,5)}`);
